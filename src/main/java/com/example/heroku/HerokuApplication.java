f0c776a18a450b22717a01233dd768f24d1be071
Lukas Juodeika <lukas@ito.lt>
2020-11-12 23:53:24 +0200

    Update to jdk 11

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -5,7 +5,6 @@ import com.example.heroku.entities.Role;
 import com.example.heroku.entities.UserEntity;
 import com.example.heroku.repositories.OffersRepository;
 import com.example.heroku.services.UserService;
-import org.apache.tomcat.util.bcel.Const;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,12 +12,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Set;
 
-@RestController
 @SpringBootApplication
 public class HerokuApplication {
 
@@ -27,16 +23,6 @@ public class HerokuApplication {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(HerokuApplication.class, args);
-    }
-
-    @GetMapping(value = "/")
-    public String index() {
-        return "Hello world";
-    }
-
-    @GetMapping(value = "/private")
-    public String privateArea() {
-        return "Private area";
     }
 
     @Bean
